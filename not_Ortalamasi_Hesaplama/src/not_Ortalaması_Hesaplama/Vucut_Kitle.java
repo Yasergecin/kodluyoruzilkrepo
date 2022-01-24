b025e1d2f22775c346bba8b40a80a57ce9ab6346
Yasin Gecer <yasin.gecer@hotmail.com>
2022-01-24 15:38:22 +0200

    Vücut kitle indeksi eklendi

---
+++ not_Ortalamasi_Hesaplama/src/not_Ortalaması_Hesaplama/Vucut_Kitle.java
@@ -0,0 +1,21 @@
+package not_Ortalamasý_Hesaplama;
+
+import java.util.Scanner;
+
+public class Vucut_Kitle {
+
+	public static void main(String[] args) {
+		double kilo,boy, index;
+		Scanner in = new Scanner(System.in);
+		
+		System.out.println("Kilonuzu giriniz: ");
+		kilo = in.nextDouble();
+		System.out.println("Boyunuzu (metre cinsinden) giriniz: ");
+		boy=in.nextDouble();
+		index= kilo/(boy*boy);
+		System.out.println("Vücut Kitle Ýndeksiniz : "+index);
+		
+
+	}
+
+}
